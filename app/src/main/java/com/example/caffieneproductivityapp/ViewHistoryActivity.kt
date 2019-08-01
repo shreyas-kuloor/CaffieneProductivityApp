125dbb8f6fbed8fb42fc8a8f9d3d2521c2c4083d
Shreyas Kuloor <shreyas.kuloor@willowtreeapps.com>
2019-08-01 13:09:37 -0400

    Add weekly filter to rating productivity chart

--- app/src/main/java/com/example/caffieneproductivityapp/ViewHistoryActivity.kt
+++ app/src/main/java/com/example/caffieneproductivityapp/ViewHistoryActivity.kt
@@ -23,7 +23,9 @@ import com.anychart.scales.DateTime
 import com.example.caffieneproductivityapp.persistence.DrinkRatingViewModel
 import com.example.caffieneproductivityapp.persistence.Rating
 import com.google.android.material.navigation.NavigationView
+import java.util.*
 import java.util.stream.Collectors
+import kotlin.collections.ArrayList
 
 
 class ViewHistoryActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -46,51 +48,11 @@ class ViewHistoryActivity : AppCompatActivity(), NavigationView.OnNavigationItem
 
         navView.setNavigationItemSelectedListener(this)
 
-        val chartView : AnyChartView = findViewById(R.id.any_chart_view)
-        val chart : Cartesian = AnyChart.line()
-        chart.title("Productivity Over Time")
-        chart.yAxis(0).title("Rating")
-        chart.yScale().minimum(0)
-        chart.yScale().maximum(5)
-        chart.yScale().ticks().allowFractional(false)
-        chart.xAxis(0).title("Time")
-        val dateTimeScale = DateTime.instantiate()
-        val currentTime = System.currentTimeMillis()
-        val midnightBefore = currentTime - currentTime % (24 * 60 * 60 * 1000)
-        val midnightAfter = midnightBefore + 24 * 60 * 60 * 1000
-        dateTimeScale.ticks().interval(0,0,0,1,0, 0)
-        dateTimeScale.minimum(midnightBefore)
-        dateTimeScale.maximum(midnightAfter)
-        chart.xScale(dateTimeScale)
-
         drinkRatingViewModel = ViewModelProviders.of(this).get(DrinkRatingViewModel::class.java)
         drinkRatingViewModel.allRatings.observe(this, Observer { ratings ->
             ratingsList = ratings
             println("SIZE =====" + ratingsList.size)
-
-            val timestamps : List<Long> = ratingsList.stream().map(Rating::timeStamp).collect(Collectors.toList())
-            val productivities : List<Int> = ratingsList.stream().map(Rating::productivityRating).collect(Collectors.toList())
-
-
-            val data : ArrayList<DataEntry> = ArrayList()
-            val maxIndex = timestamps.size - 1
-            for (i in 0..maxIndex) {
-                val timestampsAdjusted = timestamps[i]-14400000
-                if (timestampsAdjusted in midnightBefore..midnightAfter) {
-                    data.add(ValueDataEntry(timestampsAdjusted, productivities[i]))
-                }
-            }
-
-            val series : Line = chart.line(data)
-            series.name("Productivity Rating")
-            series.hovered().markers().enabled(true)
-            series.hovered().markers().type(MarkerType.CIRCLE).size(4)
-            series.tooltip().position("right").anchor(Anchor.LEFT_CENTER).offsetX(5).offsetY(5)
-
-            chartView.setChart(chart)
         })
-
-
 
     }
 
@@ -113,12 +75,89 @@ class ViewHistoryActivity : AppCompatActivity(), NavigationView.OnNavigationItem
         // Handle action bar drink_item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        val settingsIntent = Intent(this, SettingsActivity::class.java)
-        startActivity(settingsIntent)
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        when (item.itemId) {
+            R.id.action_settings -> {
+                val settingsIntent = Intent(this, SettingsActivity::class.java)
+                startActivity(settingsIntent)
+                return true
+            }
+            R.id.daily_filter -> {
+                item.isChecked = true
+                println("SIZE=========" + ratingsList.size)
+                val chartView : AnyChartView = findViewById(R.id.any_chart_view)
+                val chart : Cartesian = AnyChart.line()
+                initChart(chart)
+                setChartLimits(chart, ChartType.Daily)
+                val timestamps : List<Long> = ratingsList.stream().map(Rating::timeStamp).collect(Collectors.toList())
+                val productivities : List<Int> = ratingsList.stream().map(Rating::productivityRating).collect(Collectors.toList())
+
+                val data : ArrayList<DataEntry> = ArrayList()
+                val currentTime = System.currentTimeMillis()
+                val midnightBefore = currentTime - currentTime % (24 * 60 * 60 * 1000)
+                val midnightAfter = midnightBefore + 24 * 60 * 60 * 1000
+                val maxIndex = timestamps.size - 1
+                for (i in 0..maxIndex) {
+                    val timestampsAdjusted = timestamps[i]-14400000
+                    if (timestampsAdjusted in midnightBefore..midnightAfter) {
+                        data.add(ValueDataEntry(timestampsAdjusted, productivities[i]))
+                    }
+                }
+                val series : Line = chart.line(data)
+                series.name("Productivity Rating")
+                series.hovered().markers().enabled(true)
+                series.hovered().markers().type(MarkerType.CIRCLE).size(4)
+                series.tooltip().position("right").anchor(Anchor.LEFT_CENTER).offsetX(5).offsetY(5)
+
+                chartView.setChart(chart)
+            }
+            R.id.weekly_filter -> {
+                item.isChecked = true
+                println("SIZE=========" + ratingsList.size)
+                val chartView : AnyChartView = findViewById(R.id.any_chart_view)
+                val chart : Cartesian = AnyChart.line()
+                initChart(chart)
+                setChartLimits(chart, ChartType.Weekly)
+                val timestamps : List<Long> = ratingsList.stream().map(Rating::timeStamp).collect(Collectors.toList())
+                val productivities : List<Int> = ratingsList.stream().map(Rating::productivityRating).collect(Collectors.toList())
+
+
+                val data : ArrayList<DataEntry> = ArrayList()
+                var cal : Calendar = Calendar.getInstance()
+                cal.set(Calendar.HOUR_OF_DAY, 0)
+                cal.clear(Calendar.MINUTE)
+                cal.clear(Calendar.SECOND)
+                cal.clear(Calendar.MILLISECOND)
+                cal.set(Calendar.DAY_OF_WEEK, 1)
+                val firstDayOfWeek = cal.timeInMillis
+
+                cal.add(Calendar.WEEK_OF_YEAR, 1)
+                cal.add(Calendar.MILLISECOND, -1)
+                val lastDayOfWeek = cal.timeInMillis
+                val maxIndex = timestamps.size - 1
+                for (i in 0..maxIndex) {
+                    val timestampsAdjusted = timestamps[i]-14400000
+                    if (timestampsAdjusted in firstDayOfWeek..lastDayOfWeek) {
+                        data.add(ValueDataEntry(timestampsAdjusted, productivities[i]))
+                    }
+                }
+
+                val series : Line = chart.line(data)
+                series.name("Productivity Rating")
+                series.hovered().markers().enabled(true)
+                series.hovered().markers().type(MarkerType.CIRCLE).size(4)
+                series.tooltip().position("right").anchor(Anchor.LEFT_CENTER).offsetX(5).offsetY(5)
+
+                chartView.setChart(chart)
+            }
+            R.id.monthly_filter -> {
+                item.isChecked = true
+            }
+            R.id.yearly_filter -> {
+                item.isChecked = true
+            }
         }
+        return false
+
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
@@ -147,4 +186,66 @@ class ViewHistoryActivity : AppCompatActivity(), NavigationView.OnNavigationItem
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
     }
+
+    private fun initChart(chart: Cartesian) {
+        chart.apply {
+            title("Daily Productivity Over Time")
+            yAxis(0).title("Rating")
+            yScale().minimum(0)
+            yScale().maximum(5)
+            yScale().ticks().allowFractional(false)
+            xAxis(0).title("Time")
+        }
+    }
+
+    private fun setChartLimits(chart: Cartesian, type: ChartType) {
+        when (type) {
+            ChartType.Daily -> {
+                val dateTimeScale = DateTime.instantiate()
+                val currentTime = System.currentTimeMillis()
+                val midnightBefore = currentTime - currentTime % (24 * 60 * 60 * 1000)
+                val midnightAfter = midnightBefore + 24 * 60 * 60 * 1000
+                dateTimeScale.ticks().interval(0,0,0,1,0, 0)
+                dateTimeScale.minimum(midnightBefore)
+                dateTimeScale.maximum(midnightAfter)
+                chart.xScale(dateTimeScale)
+            }
+            ChartType.Weekly -> {
+                val dateTimeScale = DateTime.instantiate()
+                var cal : Calendar = Calendar.getInstance()
+                cal.firstDayOfWeek
+                cal.set(Calendar.HOUR_OF_DAY, 0)
+                cal.clear(Calendar.MINUTE)
+                cal.clear(Calendar.SECOND)
+                cal.clear(Calendar.MILLISECOND)
+                cal.set(Calendar.DAY_OF_WEEK, cal.firstDayOfWeek)
+                val firstDayOfWeek = cal.timeInMillis
+
+                cal.add(Calendar.WEEK_OF_YEAR, 1)
+                cal.add(Calendar.MILLISECOND, -1)
+                val lastDayOfWeek = cal.timeInMillis
+
+                dateTimeScale.ticks().interval(0,0,1,0,0, 0)
+                dateTimeScale.minimum(firstDayOfWeek)
+                dateTimeScale.maximum(lastDayOfWeek)
+                chart.xScale(dateTimeScale)
+            }
+            ChartType.Monthly -> {
+
+            }
+            ChartType.Yearly -> {
+
+            }
+        }
+    }
+
+    private enum class ChartType {
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly
+    }
+
 }
+
+
